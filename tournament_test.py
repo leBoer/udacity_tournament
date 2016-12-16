94bf7b358a9897214ed28fc2e098eefbb1221efc
leBoer <christianborre@gmail.com>
2016-12-16 11:57:31 +0100

    Opens up all tests in tournament_test.py

--- tournament_test.py
+++ tournament_test.py
@@ -153,5 +153,5 @@ if __name__ == '__main__':
     testCount()
     testStandingsBeforeMatches()
     testReportMatches()
-    # testPairings()
+    testPairings()
     print "Success!  All tests pass!"
